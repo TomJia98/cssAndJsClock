b44443b1f29a6fcf5a43e9b22e18c01dde4975b0
tomjia98 <tomchristian1998@gmail.com>
2023-07-01 15:30:41 +0800

    uncomplicated the caculations and fixed skipping

--- script.js
+++ script.js
@@ -8,6 +8,7 @@ let minute = d.getMinutes();
 let hour = d.getHours();
 
 let convertToSeconds = hour * 60 * 60 + minute * 60 + second;
+console.log(convertToSeconds);
 // setting the data attribute of the current time digitally
 secondHand.setAttribute("data-init", second);
 minuteHand.setAttribute("data-init", minute);
@@ -16,26 +17,29 @@ hourHand.setAttribute("data-init", hour);
 // setting it on the analog hands
 const calcAnalog = (totalSeconds) => {
   const offset = 270;
-  if (totalSeconds > 86400) {
-    totalSeconds = totalSeconds - 86400;
-  }
-  let hours = Math.floor(totalSeconds / 60 / 60);
-  totalSeconds = totalSeconds - hours * 60 * 60;
-  let minutes = Math.floor(totalSeconds / 60);
-  totalSeconds = totalSeconds - minute * 60;
-  let seconds = totalSeconds;
-  let analogSeconds = seconds * 6;
-  let analogMinutes = (minutes + seconds / 60) * 6;
-  let analogHours = (hours + (minutes + seconds / 60) / 60) * 30;
-  if (analogSeconds > 360 + offset) {
-    analogSeconds -= 360;
-  }
-  if (analogMinutes > 360 + offset) {
-    analogMinutes -= 360;
-  }
-  if (analogHours > 360 + offset) {
-    analogHours -= 360;
-  }
+  //   let hours = Math.floor(totalSeconds / 60 / 60);
+  //   let hoursFloat = totalSeconds / 3600;
+  //   totalSeconds = totalSeconds - hours * 60 * 60;
+  //   let minutes = Math.floor(totalSeconds / 60);
+  //   let minuteFloat = totalSeconds / 60;
+  //   totalSeconds = totalSeconds - minutes * 60;
+  //   let seconds = totalSeconds;
+  //   let analogSeconds = seconds * 6;
+  //   let analogMinutes = (minuteFloat + seconds / 60) * 6;
+  //   let analogHours = (hoursFloat + (minuteFloat + seconds / 60) / 60) * 30;
+  let analogSeconds = totalSeconds * 6;
+  let analogMinutes = (totalSeconds / 60) * 6;
+  let analogHours = (totalSeconds / 3600) * 30;
+
+  //   if (analogSeconds > 360 + offset) {
+  //     analogSeconds -= 360;
+  //   }
+  //   if (analogMinutes > 360 + offset) {
+  //     analogMinutes -= 360;
+  //   }
+  //   if (analogHours > 360 + offset) {
+  //     analogHours -= 360;
+  //   }
   secondHand.style.transform = `rotateZ(${analogSeconds + offset}deg)`;
   minuteHand.style.transform = `rotateZ(${analogMinutes + offset}deg)`;
   hourHand.style.transform = `rotateZ(${analogHours + offset}deg)`;
@@ -47,6 +51,10 @@ let index = 0;
 setInterval(
   (countdown = () => {
     const newSeconds = convertToSeconds + index;
+    if (newSeconds >= 86400) {
+      newSeconds = 0;
+      index = 0;
+    }
     calcAnalog(newSeconds);
     index++;
   }),
